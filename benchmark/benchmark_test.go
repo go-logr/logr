2a44646912e06515abcbeb57e26c363b2b52acfb
Tim Hockin <thockin@google.com>
2021-04-22 11:11:26 -0700

    Make the benchmark not inline ther impl
    In real code, the definition of the Logger would be far away from the
    logging call-sites.  The benchmark now forces the compiler to NOT
    inline, even though it could.  It also now tests logr.Discard() and
    funcr.

--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -27,17 +27,15 @@ import (
 func noop(prefix, args string) {
 }
 
-func BenchmarkInfoOneArg(b *testing.B) {
-	var log logr.Logger = funcr.New(noop, funcr.Options{})
-
+//go:noinline
+func doInfoOneArg(b *testing.B, log logr.Logger) {
 	for i := 0; i < b.N; i++ {
 		log.Info("this is", "a", "string")
 	}
 }
 
-func BenchmarkInfoSeveralArgs(b *testing.B) {
-	var log logr.Logger = funcr.New(noop, funcr.Options{})
-
+//go:noinline
+func doInfoSeveralArgs(b *testing.B, log logr.Logger) {
 	for i := 0; i < b.N; i++ {
 		log.Info("multi",
 			"bool", true, "string", "str", "int", 42,
@@ -45,9 +43,8 @@ func BenchmarkInfoSeveralArgs(b *testing.B) {
 	}
 }
 
-func BenchmarkV0Info(b *testing.B) {
-	var log logr.Logger = funcr.New(noop, funcr.Options{})
-
+//go:noinline
+func doV0Info(b *testing.B, log logr.Logger) {
 	for i := 0; i < b.N; i++ {
 		log.V(0).Info("multi",
 			"bool", true, "string", "str", "int", 42,
@@ -55,9 +52,8 @@ func BenchmarkV0Info(b *testing.B) {
 	}
 }
 
-func BenchmarkV9Info(b *testing.B) {
-	var log logr.Logger = funcr.New(noop, funcr.Options{})
-
+//go:noinline
+func doV9Info(b *testing.B, log logr.Logger) {
 	for i := 0; i < b.N; i++ {
 		log.V(9).Info("multi",
 			"bool", true, "string", "str", "int", 42,
@@ -65,9 +61,8 @@ func BenchmarkV9Info(b *testing.B) {
 	}
 }
 
-func BenchmarkError(b *testing.B) {
-	var log logr.Logger = funcr.New(noop, funcr.Options{})
-
+//go:noinline
+func doError(b *testing.B, log logr.Logger) {
 	err := fmt.Errorf("error message")
 	for i := 0; i < b.N; i++ {
 		log.Error(err, "multi",
@@ -76,20 +71,88 @@ func BenchmarkError(b *testing.B) {
 	}
 }
 
-func BenchmarkWithValues(b *testing.B) {
-	var log logr.Logger = funcr.New(noop, funcr.Options{})
-
+//go:noinline
+func doWithValues(b *testing.B, log logr.Logger) {
 	for i := 0; i < b.N; i++ {
 		l := log.WithValues("k1", "v1", "k2", "v2")
 		_ = l
 	}
 }
 
-func BenchmarkWithName(b *testing.B) {
-	var log logr.Logger = funcr.New(noop, funcr.Options{})
-
+//go:noinline
+func doWithName(b *testing.B, log logr.Logger) {
 	for i := 0; i < b.N; i++ {
 		l := log.WithName("name")
 		_ = l
 	}
+}
+
+func BenchmarkDiscardInfoOneArg(b *testing.B) {
+	var log logr.Logger = logr.Discard()
+	doInfoOneArg(b, log)
+}
+
+func BenchmarkDiscardInfoSeveralArgs(b *testing.B) {
+	var log logr.Logger = logr.Discard()
+	doInfoSeveralArgs(b, log)
+}
+
+func BenchmarkDiscardV0Info(b *testing.B) {
+	var log logr.Logger = logr.Discard()
+	doV0Info(b, log)
+}
+
+func BenchmarkDiscardV9Info(b *testing.B) {
+	var log logr.Logger = logr.Discard()
+	doV9Info(b, log)
+}
+
+func BenchmarkDiscardError(b *testing.B) {
+	var log logr.Logger = logr.Discard()
+	doError(b, log)
+}
+
+func BenchmarkDiscardWithValues(b *testing.B) {
+	var log logr.Logger = logr.Discard()
+	doWithValues(b, log)
+}
+
+func BenchmarkDiscardWithName(b *testing.B) {
+	var log logr.Logger = logr.Discard()
+	doWithName(b, log)
+}
+
+func BenchmarkFuncrInfoOneArg(b *testing.B) {
+	var log logr.Logger = funcr.New(noop, funcr.Options{})
+	doInfoOneArg(b, log)
+}
+
+func BenchmarkFuncrInfoSeveralArgs(b *testing.B) {
+	var log logr.Logger = funcr.New(noop, funcr.Options{})
+	doInfoSeveralArgs(b, log)
+}
+
+func BenchmarkFuncrV0Info(b *testing.B) {
+	var log logr.Logger = funcr.New(noop, funcr.Options{})
+	doV0Info(b, log)
+}
+
+func BenchmarkFuncrV9Info(b *testing.B) {
+	var log logr.Logger = funcr.New(noop, funcr.Options{})
+	doV9Info(b, log)
+}
+
+func BenchmarkFuncrError(b *testing.B) {
+	var log logr.Logger = funcr.New(noop, funcr.Options{})
+	doError(b, log)
+}
+
+func BenchmarkFuncrWithValues(b *testing.B) {
+	var log logr.Logger = funcr.New(noop, funcr.Options{})
+	doWithValues(b, log)
+}
+
+func BenchmarkFuncrWithName(b *testing.B) {
+	var log logr.Logger = funcr.New(noop, funcr.Options{})
+	doWithName(b, log)
 }
