15d8fad76c0d480b97d640b56579856b02192fdd
Patrick Ohly <patrick.ohly@intel.com>
2021-09-20 10:17:25 +0200

    LogObject: optional interface for logging objects differently
    The intention is to use this when the output is structured (like JSON) when the
    original type would be logged as string. It also has some other use cases.
    
    This approach was chosen instead of a full marshaler API as in
    zapcore.ObjectMarshaler because the implementation is simpler. The overhead for
    large types is expected to be higher, but it is not certain yet whether this is
    relevant in practice. If it is, then a marshaler API can still be added later.

---
+++ example_marshaler_test.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2021 The logr Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package logr_test
+
+import (
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/logr/funcr"
+)
+
+// NewStdoutLogger returns a logr.Logger that prints to stdout.
+func NewStdoutLogger() logr.Logger {
+	return funcr.New(func(prefix, args string) {
+		if prefix != "" {
+			_ = fmt.Sprintf("%s: %s\n", prefix, args)
+		} else {
+			fmt.Println(args)
+		}
+	}, funcr.Options{})
+}
+
+// ObjectRef references a Kubernetes object
+type ObjectRef struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace,omitempty"`
+}
+
+func (ref ObjectRef) String() string {
+	if ref.Namespace != "" {
+		return ref.Namespace + "/" + ref.Name
+	}
+	return ref.Name
+}
+
+func (ref ObjectRef) MarshalLog() interface{} {
+	// We implement fmt.Stringer for non-structured logging, but we want the
+	// raw struct when using structured logs.  Some logr implementations call
+	// String if it is present, so we want to convert this struct to something
+	// that doesn't have that method.
+	type forLog ObjectRef // methods do not survive type definitions
+	return forLog(ref)
+}
+
+var _ logr.Marshaler = ObjectRef{}
+
+func ExampleMarshaler() {
+	l := NewStdoutLogger()
+	pod := ObjectRef{Namespace: "kube-system", Name: "some-pod"}
+	l.Info("as string", "pod", pod.String())
+	l.Info("as struct", "pod", pod)
+	// Output:
+	// "level"=0 "msg"="as string" "pod"="kube-system/some-pod"
+	// "level"=0 "msg"="as struct" "pod"={"name":"some-pod","namespace":"kube-system"}
+}
