a9e0cd710516c91ec3bebf1a0aa263073cdbf69b
Tim Hockin <thockin@google.com>
2021-10-04 11:34:30 -0700

    Slightly better godocs and examples
    What `godoc` renders is not quite the same as what pkg.go.dev renders
    wrt whole-file examples and output comments, but this seems better
    all around.

--- example_marshaler_test.go
+++ example_marshaler_test.go
@@ -17,22 +17,8 @@ limitations under the License.
 package logr_test
 
 import (
-	"fmt"
-
 	"github.com/go-logr/logr"
-	"github.com/go-logr/logr/funcr"
 )
-
-// NewStdoutLogger returns a logr.Logger that prints to stdout.
-func NewStdoutLogger() logr.Logger {
-	return funcr.New(func(prefix, args string) {
-		if prefix != "" {
-			_ = fmt.Sprintf("%s: %s\n", prefix, args)
-		} else {
-			fmt.Println(args)
-		}
-	}, funcr.Options{})
-}
 
 // ObjectRef references a Kubernetes object
 type ObjectRef struct {
--- funcr/example_test.go
+++ funcr/example_test.go
@@ -18,7 +18,6 @@ package funcr_test
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/logr/funcr"
@@ -34,82 +33,4 @@ func ExampleUnderlier() {
 		fn("hello", "world")
 	}
 	// Output: hello world
-}
-
-// NewStdoutLogger returns a logr.Logger that prints to stdout.
-// It demonstrates how to implement a custom With* function which
-// controls whether INFO or ERROR are printed in front of the log
-// message.
-func NewStdoutLogger() logr.Logger {
-	l := &stdoutlogger{
-		Formatter: funcr.NewFormatter(funcr.Options{}),
-	}
-	return logr.New(l)
-}
-
-type stdoutlogger struct {
-	funcr.Formatter
-	logMsgType bool
-}
-
-func (l stdoutlogger) WithName(name string) logr.LogSink {
-	l.Formatter.AddName(name)
-	return &l
-}
-
-func (l stdoutlogger) WithValues(kvList ...interface{}) logr.LogSink {
-	l.Formatter.AddValues(kvList)
-	return &l
-}
-
-func (l stdoutlogger) WithCallDepth(depth int) logr.LogSink {
-	l.Formatter.AddCallDepth(depth)
-	return &l
-}
-
-func (l stdoutlogger) Info(level int, msg string, kvList ...interface{}) {
-	prefix, args := l.FormatInfo(level, msg, kvList)
-	l.write("INFO", prefix, args)
-}
-
-func (l stdoutlogger) Error(err error, msg string, kvList ...interface{}) {
-	prefix, args := l.FormatError(err, msg, kvList)
-	l.write("ERROR", prefix, args)
-}
-
-func (l stdoutlogger) write(msgType, prefix, args string) {
-	var parts []string
-	if l.logMsgType {
-		parts = append(parts, msgType)
-	}
-	if prefix != "" {
-		parts = append(parts, prefix)
-	}
-	parts = append(parts, args)
-	fmt.Println(strings.Join(parts, ": "))
-}
-
-// WithLogMsgType returns a copy of the logger with new settings for
-// logging the message type. It returns the original logger if the
-// underlying LogSink is not a stdoutlogger.
-func WithLogMsgType(log logr.Logger, logMsgType bool) logr.Logger {
-	if l, ok := log.GetSink().(*stdoutlogger); ok {
-		clone := *l
-		clone.logMsgType = logMsgType
-		log = log.WithSink(&clone)
-	}
-	return log
-}
-
-// Assert conformance to the interfaces.
-var _ logr.LogSink = &stdoutlogger{}
-var _ logr.CallDepthLogSink = &stdoutlogger{}
-
-func ExampleFormatter() {
-	l := NewStdoutLogger()
-	l.Info("no message type")
-	WithLogMsgType(l, true).Info("with message type")
-	// Output:
-	// "level"=0 "msg"="no message type"
-	// INFO: "level"=0 "msg"="with message type"
 }
