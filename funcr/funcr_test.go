a8d7af6a31197f1e21ec61fc4eb6a11b7fc189db
James Chacon <james.chacon@snowflake.com>
2022-11-10 10:25:17 -0800

    Remove dup test

--- funcr/funcr_test.go
+++ funcr/funcr_test.go
@@ -766,11 +766,6 @@ func TestRender(t *testing.T) {
 		args:       makeKV("key", &Trawjson{}),
 		expectKV:   `"key"={"message":[]}`,
 		expectJSON: `{"key":{"message":null}}`,
-	}, {
-		name:       "byte array not json.RawMessage",
-		args:       makeKV("key", []byte{1, 2, 3, 4}),
-		expectKV:   `"key"=[1,2,3,4]`,
-		expectJSON: `{"key":[1,2,3,4]}`,
 	}}
 
 	for _, tc := range testCases {
