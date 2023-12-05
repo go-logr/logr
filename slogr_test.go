b228ba8636d02f868be6802747693a8489b182e2
Tim Hockin <thockin@google.com>
2023-12-05 10:22:26 -0800

    Break examples to new file

--- slogr_test.go
+++ slogr_test.go
@@ -17,22 +17,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package logr_test
+package logr
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"log/slog"
-	"os"
 	"path"
 	"runtime"
 	"strings"
 	"testing"
 
-	"github.com/go-logr/logr"
-	"github.com/go-logr/logr/funcr"
 	"github.com/go-logr/logr/internal/testhelp"
 )
 
@@ -46,52 +42,13 @@ var debugWithoutTime = &slog.HandlerOptions{
 	Level: slog.LevelDebug,
 }
 
-func ExampleFromSlogHandler() {
-	logrLogger := logr.FromSlogHandler(slog.NewTextHandler(os.Stdout, debugWithoutTime))
-
-	logrLogger.Info("hello world")
-	logrLogger.Error(errors.New("fake error"), "ignore me")
-	logrLogger.WithValues("x", 1, "y", 2).WithValues("str", "abc").WithName("foo").WithName("bar").V(4).Info("with values, verbosity and name")
-
-	// Output:
-	// level=INFO msg="hello world"
-	// level=ERROR msg="ignore me" err="fake error"
-	// level=DEBUG msg="with values, verbosity and name" x=1 y=2 str=abc logger=foo/bar
-}
-
-func ExampleToSlogHandler() {
-	funcrLogger := funcr.New(func(prefix, args string) {
-		if prefix != "" {
-			fmt.Fprintln(os.Stdout, prefix, args)
-		} else {
-			fmt.Fprintln(os.Stdout, args)
-		}
-	}, funcr.Options{
-		Verbosity: 10,
-	})
-
-	slogLogger := slog.New(logr.ToSlogHandler(funcrLogger))
-	slogLogger.Info("hello world")
-	slogLogger.Error("ignore me", "err", errors.New("fake error"))
-	slogLogger.With("x", 1, "y", 2).WithGroup("group").With("str", "abc").Warn("with values and group")
-
-	slogLogger = slog.New(logr.ToSlogHandler(funcrLogger.V(int(-slog.LevelDebug))))
-	slogLogger.Info("info message reduced to debug level")
-
-	// Output:
-	// "level"=0 "msg"="hello world"
-	// "msg"="ignore me" "error"=null "err"="fake error"
-	// "level"=0 "msg"="with values and group" "x"=1 "y"=2 "group"={"str"="abc"}
-	// "level"=4 "msg"="info message reduced to debug level"
-}
-
 func TestWithCallDepth(t *testing.T) {
 	debugWithCaller := *debugWithoutTime
 	debugWithCaller.AddSource = true
 	var buffer bytes.Buffer
-	logger := logr.FromSlogHandler(slog.NewTextHandler(&buffer, &debugWithCaller))
+	logger := FromSlogHandler(slog.NewTextHandler(&buffer, &debugWithCaller))
 
-	logHelper := func(logger logr.Logger) {
+	logHelper := func(logger Logger) {
 		logger.WithCallDepth(1).Info("hello")
 	}
 
@@ -108,31 +65,31 @@ func TestRunSlogTestsOnSlogSink(t *testing.T) {
 	// This proves that slogSink passes slog's own tests.
 	testhelp.RunSlogTests(t, func(buffer *bytes.Buffer) slog.Handler {
 		handler := slog.NewJSONHandler(buffer, nil)
-		logger := logr.FromSlogHandler(handler)
-		return logr.ToSlogHandler(logger)
+		logger := FromSlogHandler(handler)
+		return ToSlogHandler(logger)
 	})
 }
 
 func TestSlogSinkOnDiscard(_ *testing.T) {
 	// Compile-test
-	logger := slog.New(logr.ToSlogHandler(logr.Discard()))
+	logger := slog.New(ToSlogHandler(Discard()))
 	logger.WithGroup("foo").With("x", 1).Info("hello")
 }
 
 func TestConversion(t *testing.T) {
-	d := logr.Discard()
-	d2 := logr.FromSlogHandler(logr.ToSlogHandler(d))
+	d := Discard()
+	d2 := FromSlogHandler(ToSlogHandler(d))
 	expectEqual(t, d, d2)
 
-	e := logr.Logger{}
-	e2 := logr.FromSlogHandler(logr.ToSlogHandler(e))
+	e := Logger{}
+	e2 := FromSlogHandler(ToSlogHandler(e))
 	expectEqual(t, e, e2)
 
 	text := slog.NewTextHandler(io.Discard, nil)
-	text2 := logr.ToSlogHandler(logr.FromSlogHandler(text))
+	text2 := ToSlogHandler(FromSlogHandler(text))
 	expectEqual(t, text, text2)
 
-	text3 := logr.ToSlogHandler(logr.FromSlogHandler(text).V(1))
+	text3 := ToSlogHandler(FromSlogHandler(text).V(1))
 	if handler, ok := text3.(interface {
 		GetLevel() slog.Level
 	}); ok {
